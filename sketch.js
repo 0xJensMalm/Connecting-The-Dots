56b0d748065cab1c2b0d3929a411d4ab8812b049
0xJensMalm <112858577+0xJensMalm@users.noreply.github.com>
2024-04-10 11:20:38 +0200

    added autoRotation

--- sketch.js
+++ sketch.js
@@ -1,25 +1,32 @@
 const sketchConfig = {
   canvasSize: { width: 800, height: 800 },
   zoom: { min: 0.5, max: 5, initial: 2.5, sensitivity: 1000 },
-  gridColor: [0, 255, 255],
+  gridColor: [204, 255, 51],
   pointGridSize: 0.4,
   vertexDistanceThreshold: 10,
   modelRotation: { x: 90, y: 0, z: 100 }, // Rotation angles in degrees
-  // x180 y180 z0
+  autoRotation: 10,
+  // face: x: 180 y: 180 z: 0
+  // fly: x: 90, y: 0, z: 100
 };
 
 const particleConfig = {
-  numParticles: 200,
+  numParticles: 2500,
   size: 0.5,
   lineStrokeSize: 0.2,
   linesEnabled: false,
-  lineLifespan: 200,
+  lineLifespan: 500,
   pathLength: 30, //distance threshold to next target
   particleSpeed: 1,
-  colors: [
+  /*colors: [
     [255, 204, 0],
     [0, 255, 255],
     [255, 105, 180],
+  ],*/
+  colors: [
+    [112, 224, 0],
+    [220, 47, 2],
+    [255, 186, 8],
   ],
 };
 
@@ -27,6 +34,7 @@ let myModel;
 let particles = [];
 let filteredVertices = [];
 let zoom = sketchConfig.zoom.initial;
+let capturer;
 
 function preload() {
   myModel = loadModel("fly.obj", true);
@@ -48,10 +56,15 @@ function draw() {
   orbitControl();
   scale(zoom);
 
-  // Apply rotation around X, Y, and Z axes
+  // Apply rotation around X and Y axes
   rotateX(radians(sketchConfig.modelRotation.x));
   rotateY(radians(sketchConfig.modelRotation.y));
-  rotateZ(radians(sketchConfig.modelRotation.z));
+
+  // Apply auto rotation around Z axis
+  let zRotation =
+    radians(sketchConfig.modelRotation.z) +
+    map(sketchConfig.autoRotation, 0, 100, 0, 0.1) * frameCount;
+  rotateZ(zRotation);
 
   displayVertices();
   updateAndDisplayParticles();
